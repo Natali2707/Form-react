8a40daa4aba7622f7a78f2856f3f49fc6b3c1e5f
Natali2707 <105042840+Natali2707@users.noreply.github.com>
2022-10-19 06:37:17 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./components/styles/userForm.module.css";
+import Notification from "./components/Notification";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class UserForm extends React.Component {
+  state = {
+    showNotification: false,
+  };
+  handleChange = (e) => {
+    const {name, value} = e.target;
+    this.setState({[name]: value});
+  }
+  submitForm = (e) => {
+e.preventDefault();
+e.target.reset();
+this.setState({showNotification: true})
+  }
+  render() {
+    const { name, surname, password, email, showNotification } = this.state;
+
+    return (
+      <>
+        <h4>Введите данные:</h4>
+        <form className={styles.form} onSubmit={this.submitForm}>
+          <input 
+          type="text" 
+          name="name" 
+          placeholder="Ваше имя"
+          onChange={this.handleChange}
+          />
+           <input 
+          type="text" 
+          name="surname" 
+          placeholder="Ваша фамилия"
+          onChange={this.handleChange}
+          />
+           <input 
+          type="password" 
+          name="password" 
+          placeholder="Ваш пароль"
+          onChange={this.handleChange}
+          />
+           <input 
+          type="email" 
+          name="email" 
+          placeholder="Ваш email"
+          onChange={this.handleChange}
+          />
+        <button className={styles.btn}>Отправить данные</button>
+        </form>
+        <br />
+        {showNotification && (
+          <Notification name={name} surname={surname} password={password} email={email}/>
+        )}
+      </>
+    );
+  }
 }
 
-export default App;
+export default UserForm;
